4f48176fe581bc644cac0bafb12062bbd2fe473f
Shraddha <snandgaonkar27@gmail.com>
2022-09-18 17:56:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {Routes,Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  pageSize =5;
+  render() {
+    
+    return (
+     
+      <div>
+      
+        <Navbar/>
+      
+        <Routes>
+          <Route exact path="/"  element={<News country="in" pageSize={this.pageSize} key="genral" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="general" />}/>
+          <Route exact path="/business"  element={< News country="in" pageSize={this.pageSize} key="business" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="business" />}/>
+          <Route exact path="/entertainment"  element={<News country="in" pageSize={this.pageSize} key="entertainment" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="entertainment" />}/>
+          <Route exact path="/health" element={<News country="in" pageSize={this.pageSize} key="health" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="health"/> }/>
+          <Route exact path="/science"   element={<News country="in" pageSize={this.pageSize} key="science" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="science" />}/>
+          <Route exact path="/sports"  element={<News country="in" pageSize={this.pageSize} key="sports" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="sports" />}/>
+          <Route exact path="/technology"  element={<News country="in" pageSize={this.pageSize} key="technology" apiKey="08bb9bd7551d4a439d23144f5b847d7b" category="technology" />}/>
+        </Routes>
+ 
+      </div>
+      
+    )
+  }
 }
-
-export default App;
